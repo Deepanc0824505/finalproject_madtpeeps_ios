6c32b42e954042ebcc835e6bd900cc20e3ca520c
c0832707 <97565518+Asky707@users.noreply.github.com>
2022-01-27 14:24:14 -0500

    add handlers to addtaskcontroller
    add handlers to addtaskcontroller

--- FinalProject_MADTPEEPS_iOS/FinalProject_MADTPEEPS_iOS/Controller/AddTaskController.swift
+++ FinalProject_MADTPEEPS_iOS/FinalProject_MADTPEEPS_iOS/Controller/AddTaskController.swift
@@ -6,84 +6,75 @@
 //
 
 import UIKit
+import CoreData
+import AVFoundation
 
-class AddTaskController: UITableViewController {
-
+class AddTaskController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+    
+    @IBOutlet weak var btnSaveAudio: UIButton!
+    @IBOutlet weak var btnCancelAudio: UIButton!
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var btnPlaySaveAudio: UIButton!
+    @IBOutlet weak var btnAudioAdd: UIButton!
+    @IBOutlet weak var audioViewLayout: UIStackView!
+    @IBOutlet weak var imgListView: UICollectionView!
+    @IBOutlet weak var audioLayout: UIStackView!
+    @IBOutlet weak var btnRecord: UIButton!
+    @IBOutlet var lblTimeAudio: UILabel!
+    @IBOutlet weak var btnAdd: UIButton!
+    @IBOutlet weak var tftaskTitle: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var lblAudioName: UILabel!
+    @IBOutlet weak var norecordingLayout: UIStackView!
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var selectedTask: Task? {
+        didSet {
+            editMode = true
+        }
+    }
+    
+    // edit mode by default is false
+    var editMode: Bool = false
+    
+    // an in instance of the noteTVC in noteVC - delegate
+    weak var delegate: TaskTVController?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        // Do any additional setup after loading the view.
+        audioLayout.isHidden = true
+        audioViewLayout.isHidden = true
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+  
+    
+    @IBAction func createButtonHandler(_ sender: UIButton) {
+        
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    
+    
+    @IBAction func addAudio(_ sender: UIButton) {
 
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    @IBAction func recordingHandler(_ sender: UIButton) {
+       
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func saveAudio(_ sender: UIButton) {
+       
     }
-    */
-
+    
+    @IBAction func playAudio(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func deleteAudio(_ sender: UIButton) {
+        
+    }
+    
 }
